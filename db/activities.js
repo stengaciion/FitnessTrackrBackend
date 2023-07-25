e23423f4fcb996b26d9fe9f3303ffc9c98155aa8
James <>
2023-07-24 21:00:02 -0500

    all activities functions pass

--- db/activities.js
+++ db/activities.js
@@ -3,21 +3,109 @@ const client = require('./client');
 // database functions
 async function createActivity({ name, description }) {
   // return the new activity
+  try {
+    const {rows: [newActivity]} = await client.query(
+      `INSERT INTO activities (name, description) 
+    VALUES ($1, $2)
+    RETURNING *;
+`,
+      [name, description]
+    );
+    return newActivity;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 async function getAllActivities() {
   // select and return an array of all activities
+   try {
+    const { rows } = await client.query(`
+  SELECT * FROM activities;`);
+    return rows;
+   } catch (error) {
+    return error
+   }
 }
 
-async function getActivityById(id) {}
+async function getActivityById(id) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+  SELECT * FROM activities
+  WHERE id=$1;`,
+      [id]
+    );
+    return activity;
+  } catch (error) {
+   return error
+  }
+}
 
-async function getActivityByName(name) {}
+async function getActivityByName(name) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+  SELECT * FROM activities
+  WHERE name=$1;`,
+      [name]
+    );
+    return activity;
+  } catch (error) {
+    return error
+  }
+}
 
 // used as a helper inside db/routines.js
 async function attachActivitiesToRoutines(routines) {}
 
 async function updateActivity({ id, ...fields }) {
+  const { name, description } = fields
   // don't try to update the id
+  try {
+    if (name && description) {
+    const { rows: [updatedActivity] } = await client.query(`
+    UPDATE activities
+    SET name=$1, description=$2
+    WHERE id=${id}
+    RETURNING *`
+    , [name, description])
+    return updatedActivity
+    } else if (name) {
+      const {
+        rows: [updatedActivity]
+      } = await client.query(
+        `
+    UPDATE activities
+    SET name=$1
+    WHERE id=${id}
+    RETURNING *`,
+        [name]
+      );
+      return updatedActivity;
+    } else if (description) {
+      const {
+        rows: [updatedActivity]
+      } = await client.query(
+        `
+    UPDATE activities
+    SET description=$1
+    WHERE id=${id}
+    RETURNING *`,
+        [description]
+      );
+      return updatedActivity;
+    } else {
+      return
+    }
+  } catch (error) {
+    return error
+  }
   // do update the name and description
   // return the updated activity
 }
