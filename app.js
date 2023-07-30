52b61b67f08ae44af86e07a5cb163a4c3cfbc504
James <>
2023-07-29 21:20:27 -0500

    server is set up and api/health route works

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
 
 // Setup your Middleware and API Router here
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+//set /api routes
+const apiRouter = require('./api')
+app.use("/api", apiRouter);
 
 module.exports = app;
